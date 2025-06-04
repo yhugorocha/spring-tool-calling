c3ffeb3a704de064599c314be77b132c18981dd3
Hugo Silva <yhugorocha@gmail.com>
2025-06-04 20:05:19 -0300

    feat: Modifying LastedstockPrice

--- src/main/java/io/github/yhugorocha/client/TwelveClient.java
+++ src/main/java/io/github/yhugorocha/client/TwelveClient.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
-import reactor.core.publisher.Mono;
 
 @Service
 public class TwelveClient {
--- src/main/java/io/github/yhugorocha/config/WalletConfig.java
+++ src/main/java/io/github/yhugorocha/config/WalletConfig.java
@@ -1,12 +1,12 @@
 package io.github.yhugorocha.config;
 
 import io.github.yhugorocha.client.TwelveClient;
+import io.github.yhugorocha.dto.DailyStockData;
 import io.github.yhugorocha.dto.ShareResponse;
 import io.github.yhugorocha.dto.StockRequest;
 import io.github.yhugorocha.dto.StockResponse;
 import io.github.yhugorocha.repository.ShareRepository;
 import io.github.yhugorocha.services.ShareService;
-import io.github.yhugorocha.services.StockService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Description;
@@ -26,6 +26,14 @@ public class WalletConfig {
     @Bean
     @Description("Lasted stock price")
     public Function<StockRequest, StockResponse> lastedStockPrice(TwelveClient twelveClient){
-        return new StockService(twelveClient);
+        return stockRequest -> {
+            var stock = twelveClient.getStock(stockRequest.company());
+            if(stock.getValues().isEmpty()){
+                throw new RuntimeException("Dados de ações não encontrados ou vazios.");
+            }
+
+            DailyStockData dailyStockData = stock.getValues().getFirst();
+            return new StockResponse(Float.parseFloat(dailyStockData.getClose()));
+        };
     }
 }
